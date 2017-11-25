50116b2f8aabbba8397c34965d3fdba2777a6cf0
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2017-11-25 16:39:35 +0100

    Improve main parser loop

--- src/agh/iisg/lab/Parser.java
+++ src/agh/iisg/lab/Parser.java
@@ -1,35 +1,51 @@
 package agh.iisg.lab;
 
 import agh.iisg.lab.legal.Chapter;
+import agh.iisg.lab.legal.LegalPartition;
+import agh.iisg.lab.legal.Section;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
+import static java.util.stream.Collectors.toList;
+
 public class Parser {
   private List<Chapter> chapters = new ArrayList<>();
 
   public Parser(List<String> lines, List<Predicate<String>> filters) {
 
-    Arrays.asList(lines.parallelStream()
-                       .filter(line -> filters.stream()
-                                              .allMatch(predicate -> predicate.test(line)))
+    Arrays.stream(lines.parallelStream()
+                       .filter(l -> filters.stream().allMatch(p -> p.test(l)))
                        .map(line -> line + "\n")
                        .reduce("", String::concat)
                        .split("Rozdział "))
-          .stream()
           .map(Chapter::new)
           .forEach(chapter -> {
             chapters.add(chapter);
 
-            List<String> chapterLines = Arrays.asList(chapter.getRawContent()
-                                                             .split("\n"));
-//            chapter.setNumber(chapterLines.remove(0));
-//            chapter.setTitle(chapterLines.remove(0));
-//
-//            chapter.setPartitions(articles);
+            ArrayList<String> chapterLines = new ArrayList(Arrays.asList(chapter.getRawContent().split("\n")));
+
+            if (chapterLines.get(0).equals("KONSTYTUCJA")) return;
+
+            chapter.setNumber(chapterLines.remove(0));
+            chapter.setTitle(chapterLines.remove(0));
+
+            // TODO:
+            // Extract section titles.
+            // Move on to the next partitions.
+            List<LegalPartition> sections = Arrays.stream(chapterLines.stream()
+                                                                      .map(line -> line + "\n")
+                                                                      .reduce("", String::concat)
+                                                                      .split("[A-ZĘÓĄŚŁŻŹĆŃ ]+\n"))
+                                                  .filter(sectionContent -> !"".equals(sectionContent))
+                                                  .map(Section::new)
+                                                  .collect(toList());
+            chapter.setPartitions(sections);
           });
+
+    int sth = 10;
   }
 
   public List<Chapter> getChapters() {
