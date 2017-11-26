e4c9126f938c4a667ec7357535d8b9790a92e204
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2017-11-26 15:45:06 +0100

    Make use of gradle, change directory structure

--- src/test/agh/iisg/lab/ParserTest.java
+++ src/test/agh/iisg/lab/ParserTest.java
@@ -1,11 +1,11 @@
 package agh.iisg.lab;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ParserTest {
