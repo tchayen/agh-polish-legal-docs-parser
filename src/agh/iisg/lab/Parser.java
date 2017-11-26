31f8cc315ddd11cfbfbb7d9d4713c6106ae2abf8
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2017-11-26 12:26:11 +0100

    Refactor articles' parser out to the common method

--- src/agh/iisg/lab/Parser.java
+++ src/agh/iisg/lab/Parser.java
@@ -5,12 +5,14 @@ import agh.iisg.lab.legal.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 
 import static java.util.stream.Collectors.toList;
 
 public class Parser {
   private List<Chapter> chapters = new ArrayList<>();
+  private AtomicInteger articleCounter = new AtomicInteger(-1);
 
   public Parser(List<String> lines, List<Predicate<String>> filters) {
     Arrays.stream(
@@ -62,31 +64,14 @@ public class Parser {
     sections.forEach(section -> {
       section.setTitle(sectionTitles.size() > 0 ? sectionTitles.remove(0) : "");
 
-      List<Legal> partitions =
-        Arrays.stream(section.getContent().split(Article.regex.pattern()))
-              .dropWhile(String::isEmpty)
-              .map(raw -> {
-                ArrayList<String> content = new ArrayList<>(
-                  Arrays.asList(raw.split("\n")));
-
-                String number = content.remove(0);
-                Enumerable partition = new Article();
-                partition.setNumber(number.substring(0, number.length() - 1));
-                partition.setContent("Art. " + raw);
-                return partition;
-              })
-              .collect(toList());
-      section.setPartitions(new ArrayList<>(partitions));
-
-      partitions.parallelStream()
-              .forEach(article -> this.getPartitions(
-                article,
-                new ArrayList<PartitionGenerator>(Arrays.asList(
-                  new PartitionGenerator(Paragraph::new, null),
-                  new PartitionGenerator(Point::new, null),
-                  new PartitionGenerator(Letter::new, null)
-                ))
-              ));
+      this.getPartitions(
+        section,
+        new ArrayList<>(Arrays.asList(
+          new PartitionGenerator(Article::new, articleCounter),
+          new PartitionGenerator(Paragraph::new, null),
+          new PartitionGenerator(Point::new, null),
+          new PartitionGenerator(Letter::new, null)
+        )));
     });
   }
 
@@ -94,9 +79,12 @@ public class Parser {
     if (generators.size() == 0) return;
 
     PartitionGenerator generator = generators.remove(0);
+    String pattern = generator.getSupplier().get().regex().pattern();
+
     List<Legal> partitions =
-      Arrays.stream(parent.getContent().split(generator.getSupplier().get().regex().pattern()))
+      Arrays.stream(parent.getContent().split(pattern))
             .dropWhile(String::isEmpty)
+            .dropWhile(line -> line.matches(pattern))
             .map(raw -> {
               Enumerable partition = (Enumerable) generator.getSupplier().get();
               partition.setNumber(Integer.toString(generator.getCounter().incrementAndGet()));
