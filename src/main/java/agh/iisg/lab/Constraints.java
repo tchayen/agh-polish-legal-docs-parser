863e5ee5da3d2817a4fc7c07b4b085cedfd949b0
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2017-12-02 01:31:40 +0100

    Prepare for simplification of class hierarchy

--- src/main/java/agh/iisg/lab/Constraints.java
+++ src/main/java/agh/iisg/lab/Constraints.java
@@ -10,17 +10,36 @@ public class Constraints {
   public static final String WORD_REGEX = "[A-zĘęÓóĄąŚśŁłŻżŹźĆćŃń, ]+";
 
   public static final List<String> splitters = Arrays.asList(
-    WORD_REGEX + "\n",
+    "\n(?=DZIAŁ [IVX]+\n"+ Constraints.WORD_REGEX + "\n)",
+    "\n(?=Rozdział ([IVX]|\\d)+\n" + Constraints.UPPERCASE_WORD_REGEX + "\n)",
+    "\n(?=[A-zĘęÓóĄąŚśŁłŻżŹźĆćŃń, ]+\n)",
+    "Art\\. \\d+\\.\n",
+    "\n\\d+\\. ",
+    "\n\\d+\\) ",
+    "\n[a-z]\\) ",
+    "\n- "
+  );
+
+  public static final List<String> titleMatchers = Arrays.asList(
+    "^DZIAŁ [IVX]+\n" + Constraints.WORD_REGEX + "\n",
+    "Rozdział ([IVX]|\\d)+\n" + Constraints.UPPERCASE_WORD_REGEX + "\n",
+    "^[A-zĘęÓóĄąŚśŁłŻżŹźĆćŃń, ]+\n",
+    "Art\\. \\d+\\.\n",
+    "^\\d+\\. ",
+    "^\\d+\\) ",
+    "^[a-z]\\) ",
+    "\n- "
+  );
+
+  private static final List<String> newLiners = Arrays.asList(
+    UPPERCASE_WORD_REGEX + "\n",
     "DZIAŁ [IVX]+",
     "Rozdział ([IVX]|\\d)+",
     "Art\\. \\d+\\.",
     "\\d+\\. ",
-    "\\d+\\) "
-  );
-
-  public static final List<String> titleMatchers = Arrays.asList(
-    "Art\\. \\d+\\.\n"
-  );
+    "\\d+\\) ",
+    "[a-z]+\\)"
+    );
 
   /**
    * Join lines with words separated by "-".
@@ -31,7 +50,7 @@ public class Constraints {
    * Replace new line with space where it is not followed by one of the non-breaking line beginnings.
    */
   public static final Pattern skipNewlines = Pattern.compile(
-    "\n(?!" + WORD_REGEX + "\n|Rozdział [IVX]+|Art\\. \\d+\\.|\\d+\\. |\\d+\\) )");
+    "\n(?!" + newLiners.stream().reduce((a, b) -> a + "|" + b).orElse("") + ")");
 
   /**
    * Replace spaces with new lines in cases where article is followed directly by plain text.
